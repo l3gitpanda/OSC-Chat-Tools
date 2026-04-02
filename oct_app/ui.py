bf7e343c81ab0c5140b0fb6c848a69781dfc3406
l3gitpanda <callofdutyguy4@gmail.com>
2026-04-02 04:32:10 -0700

    Revert "Create oct_app package scaffold and modular entrypoint"

--- oct_app/ui.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from typing import Callable
-
-
-def uiThread(builder: Callable[[], None]) -> None:
-    """FreeSimpleGUI construction + event loop callback entrypoint."""
-
-    builder()
